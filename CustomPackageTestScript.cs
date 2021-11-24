76e5e7bcd5902d686b66cdb9dd0968d03cca21a0
senneroflove <senneroflove@gmail.com>
2021-11-24 20:03:09 +0900

    ho

---
+++ CustomPackageTestScript.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Sena.Test
+{
+    public class CustomPackageTestScript : MonoBehaviour
+    {
+        void Start()
+        {
+            Debug.Log("Hello from Custom Package script");
+        }
+    }
+}
